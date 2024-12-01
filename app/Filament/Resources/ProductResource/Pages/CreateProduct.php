0090b93cc7d4977a6345e43d04591cafb7c66b5e
grajeiro123 <juniorgranjeiro.jc@gmail.com>
2024-12-01 16:13:44 -0300

    inserindo dados no formulario

--- app/Filament/Resources/ProductResource/Pages/CreateProduct.php
+++ app/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -9,4 +9,10 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateProduct extends CreateRecord
 {
     protected static string $resource = ProductResource::class;
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // dd($data);
+        $data['price'] = (str_replace(['.', ','], ['', '.'], $data['price'])) * 100;
+        return $data; 
+    }
 }
